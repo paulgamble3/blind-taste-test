7da6586bbd63c6227cae2d4fe7c39066821eefa7
Paul Gamble <paulgamble@Pauls-MacBook-Pro.local>
2023-11-02 21:24:20 -0700

    first

---
+++ components/BlindForm.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import json
+from firebase.firebase_utils import write_task_item
+import datetime
+import random
+import os
+import pickle
+
+# take the next object in the list, with the human/ai label
+# display it
+# collect feedback
+# write feedback and remove the item from the list
+# if list is empty, show a message
+
+def get_user_task_state(username, task_name):
+    fn = './data/user_task_state/' + username + '_' + task_name + '.json'
+    if os.path.exists(fn):
+        with open(fn, 'r') as f:
+            user_state = json.load(f)
+    else:
+        label_set = random.choice(['A', 'B'])
+        label_name = './data/script_sets/blind_set_' + label_set + '.pkl'
+        labels = pickle.load(open(label_name, 'rb'))
+        user_state = {
+            'username': username,
+            'task_name': task_name,
+            'task_items': labels
+        }
+        with open(fn, 'w') as f:
+            json.dump(user_state, f)
+    return user_state
+
+def get_item(user_state):
+    fn = './data/user_task_state/' + user_state['username'] + '_' + user_state['task_name'] + '.json'
+        
+    if len(user_state['task_items']) > 0:
+        with open(fn, 'r') as f:
+            user_state = json.load(f)
+        item = user_state['task_items'].pop()
+        with open(fn, 'w') as f:
+            json.dump(user_state, f)
+        return item
+    else:
+        return None
+
+
+
+def BlindForm():
+
+    def capture_score():
+        feedback_object["Overall Subjective Quality"] = st.session_state["Overall Subjective Quality"]
+        write_task_item(feedback_object, 'blind-taste-test')
+        st.session_state["Overall Subjective Quality"] = "1"
+        st.experimental_rerun()
+
+    
+    user = st.session_state.username
+    user_state = get_user_task_state(user, 'blind-taste-test')
+    item = get_item(user_state)
+    label = item[1]
+    script_fn = item[0]
+
+    feedback_object = {
+        'username': user,
+        'task_name': 'blind-taste-test',
+        'label': label,
+        'script_fn': script_fn,
+        'timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+    with open(script_fn, 'r') as f:
+        script_text = f.read()
+
+    turns = script_text.split('\n')
+
+    st.write(f"The following transcript is a conversation between a patient and **{label} nurse**. Please read the script and answer the following questions.")
+    st.write("[Somes lines may have been omitted for brevity.]")
+
+    for turn in turns:
+        st.write(turn)
+
+    st.write("Please answer the following questions about the script you just read.")
+
+    with st.form("my_form"):
+        key_name = "Overall Subjective Quality"
+        feedback_object[key_name] = st.radio("How would you rate the overall subjective quality of the nurse in this conversation?", ("1", "2","3","4","5","6","7"), key=key_name, horizontal=True)
+
+        st.form_submit_button("Submit", on_click=capture_score)
---
+++ data/script_sets/generate_script_sets.py
@@ -0,0 +1,25 @@
+import glob
+import random
+import pickle
+
+all_scripts = glob.glob("data/raw_scripts/*.txt")
+
+script_labels_A = []
+script_labels_B = []
+
+for script in all_scripts:
+    z = ["human", "AI"]
+    random.shuffle(z)
+    script_labels_A.append([script, z[0]])
+    script_labels_B.append([script, z[1]])
+
+
+random.shuffle(script_labels_A)
+random.shuffle(script_labels_B)
+
+
+with open("data/script_sets/blind_set_A.pkl", "wb") as f:
+    pickle.dump(script_labels_A, f)
+
+with open("data/script_sets/blind_set_B.pkl", "wb") as f:
+    pickle.dump(script_labels_B, f)
